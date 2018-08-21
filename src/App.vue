4546b071925e93a971dffc34b7edf1130f975b86
Jake Overall <joverall22@gmail.com>
2018-08-20 20:14:45 -0600

    init

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+    <helloworld></helloworld>
+  </div>
+</template>
+
+<script>
+  import helloworld from './components/HelloWorld.vue'
+  export default {
+    name: 'App',
+    components: {
+      helloworld
+    }
+  }
+
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="hello-world">
+    <h1 :class="status()">{{target.name}}</h1>
+    <p>{{target.health}}</p>
+    <button @click="slap">Slap</button>
+    <button @click="punch">Punch</button>
+    <button @click="pet">Pet</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'hello-world',
+    data() {
+      return {
+        target: {
+          name: "Mister Snibbly",
+          health: 100
+        },
+        attacks: {
+          slap: 1,
+          punch: 5,
+          pet: 10
+        }
+      }
+    },
+    methods: {
+      status() {
+        let className = "green"
+        if (this.target.health == 0) {
+          className = "red"
+        } else if (this.target.health <= 45) {
+          className = "orange"
+        } else if (this.target.health <= 75) {
+          className = "yellow"
+        }
+        return className
+      },
+      pet() {
+        this.target.health -= this.attacks.pet
+        this.checkHealth()
+      },
+      slap() {
+        this.target.health -= this.attacks.slap
+        this.checkHealth()
+      },
+      punch() {
+        this.target.
+        health -= this.attacks.punch
+        this.checkHealth()
+      },
+      checkHealth() {
+        if (this.target.health < 0) {
+          this.target.health = 0
+        }
+      }
+    },
+    components: {
+
+    }
+  }
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .red {
+    color: red;
+  }
+
+  .orange {
+    color: orange;
+  }
+
+  .yellow {
+    color: yellow;
+  }
+
+  .green {
+    color: green;
+  }
+
+  h1,
+  h2 {
+    font-weight: normal;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #42b983;
+  }
+
+</style>
